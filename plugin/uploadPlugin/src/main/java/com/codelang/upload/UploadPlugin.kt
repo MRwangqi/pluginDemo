df50bf89d53deb05fdea316728e77ed0c8749d67
codelang <qi.wang@tuya.com>
2022-03-07 10:55:52 +0800

    upload init

---
+++ plugin/uploadPlugin/src/main/java/com/codelang/upload/UploadPlugin.kt
@@ -0,0 +1,9 @@
+package com.codelang.upload
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class UploadPlugin : Plugin<Project> {
+    override fun apply(project: Project) {
+    }
+}
