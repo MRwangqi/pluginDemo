3da0612dd4abd185872f31d8db0fa172a06b1306
codelang <qi.wang@tuya.com>
2022-03-18 15:25:50 +0800

    mod:完善打组件

--- plugin/uploadPlugin/src/main/java/com/codelang/upload/UploadGithub.kt
+++ plugin/uploadPlugin/src/main/java/com/codelang/upload/UploadGithub.kt
@@ -2,14 +2,17 @@ package com.codelang.upload
 
 import com.android.build.gradle.LibraryExtension
 import com.codelang.upload.utils.Util
+import groovy.util.Node
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.Task
+import org.gradle.api.artifacts.Dependency
 import org.gradle.api.plugins.JavaPluginConvention
 import org.gradle.api.publish.maven.MavenPom
 import org.gradle.api.publish.maven.MavenPublication
 import org.gradle.api.tasks.bundling.Jar
 import java.io.File
+import java.io.InputStreamReader
 
 class UploadGithub : Plugin<Project> {
 
@@ -28,7 +31,11 @@ class UploadGithub : Plugin<Project> {
                         MavenPublication::class.java
                 ) { publication ->
 
-                    publication.from(project.components.findByName("release"))
+                    if (Util.isAndroidModule(project)) {
+                        publication.from(project.components.findByName("release"))
+                    } else {
+                        publication.from(project.components.findByName("java"))
+                    }
 
                     publication.groupId = "com.aa.bb"
                     publication.artifactId = "haha"
@@ -37,10 +44,12 @@ class UploadGithub : Plugin<Project> {
 
                     //pom config
                     publication.pom { pom ->
-//                        pom.developers { develops ->
-//                        }
-                        println("----> "+ pom)
+                        addDeveloper(pom)
+
+                        // todo AGP 7.0 的实践效果看，默认就会把 dependencies 下的依赖打入了 pom
+                        // todo 所以，这个地方改成，如果 hasPom 为 false 的话，则移除 dependencies
 //                        applyPomDeps(pom = pom, project = project)
+                        removePomDeps(pom)
                     }
                 }
             }
@@ -50,7 +59,18 @@ class UploadGithub : Plugin<Project> {
                     repo.url = project.file("../build/repo").toURI()
                 }
             }
+
+            project.task("upload") {
+                it.dependsOn("publishMavenPublicationToMavenRepository")
+            }
+
+            project.tasks.filter {
+                it.name == "publishMavenPublicationToMavenRepository"
+            }.firstOrNull()?.doLast {
+                println("\n可添加依赖使用：\timplementation '${"GroupId"}:${"SdkName"}:${"SdkVersion"}'\n")
+            }
         }
+
     }
 
 
@@ -66,15 +86,12 @@ class UploadGithub : Plugin<Project> {
             //Iterate over the compile dependencies (we don't want the test ones), adding a <dependency> node for each
             scopeMapping.keys.forEach { key ->
                 try {
-                    println("----> "+ key+"---"+project.configurations.getByName(key))
                     project.configurations.getByName(key).allDependencies?.forEach { dependency ->
-
-                        println("----> "+ dependency+"---")
-//                        val dependencyNode = dependenciesNode.appendNode("dependency")
-//                        dependencyNode.appendNode("groupId", dependency.group)
-//                        dependencyNode.appendNode("artifactId", dependency.name)
-//                        dependencyNode.appendNode("version", dependency.version)
-//                        dependencyNode.appendNode("scope", scopeMapping[key])
+                        val dependencyNode = dependenciesNode.appendNode("dependency")
+                        dependencyNode.appendNode("groupId", dependency.group)
+                        dependencyNode.appendNode("artifactId", dependency.name)
+                        dependencyNode.appendNode("version", dependency.version)
+                        dependencyNode.appendNode("scope", scopeMapping[key])
                     }
                 } catch (thr: Throwable) {
 
@@ -83,6 +100,30 @@ class UploadGithub : Plugin<Project> {
         }
     }
 
+    private fun removePomDeps(pom: MavenPom) {
+        pom.withXml {
+            val root = it.asNode()
+            val dependenciesNode = root.children().filter {
+                (it is Node) && it.name().toString().contains("dependencies")
+            }.firstOrNull()
+            if (dependenciesNode != null) {
+                root.remove(dependenciesNode as Node)
+            }
+        }
+    }
+
+    private fun addDeveloper(pom: MavenPom) {
+        pom.developers {
+            it.developer {
+                val p = Runtime.getRuntime().exec("git config user.email")
+                p.waitFor()
+                if (p.exitValue() == 0) {
+                    it.email.set(InputStreamReader(p.inputStream).readText().trim())
+                }
+
+            }
+        }
+    }
 
     private fun addSourceJar(project: Project): Task {
         val sourceSet = mutableSetOf<File>()
