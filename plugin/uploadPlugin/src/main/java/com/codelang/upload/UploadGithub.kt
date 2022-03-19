8d33158146addfdd31f68b7e53a1033c8c15979a
codelang <codelang29@gmail.com>
2022-03-19 20:19:23 +0800

    mod:git 相关操作

--- plugin/uploadPlugin/src/main/java/com/codelang/upload/UploadGithub.kt
+++ plugin/uploadPlugin/src/main/java/com/codelang/upload/UploadGithub.kt
@@ -2,8 +2,10 @@ package com.codelang.upload
 
 import com.android.build.gradle.LibraryExtension
 import com.codelang.upload.config.UploadConfig
+import com.codelang.upload.utils.FileUtils
 import com.codelang.upload.utils.Util
 import groovy.util.Node
+import org.eclipse.jgit.api.Git
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.Task
@@ -14,39 +16,8 @@ import org.gradle.api.tasks.bundling.Jar
 import java.io.File
 import java.io.InputStreamReader
 
-class UploadGithub : Plugin<Project> {
 
-    /**
-     * 复制一个目录及其子目录、文件到另外一个目录
-     * @param src
-     * @param dest
-     * @throws IOException
-     */
-    private fun copyFolder(src: File, dest: File) {
-        if (src.isDirectory()) {
-            if (!dest.exists()) {
-                dest.mkdir();
-            }
-            src.list()?.forEach {
-                val srcFile = File(src, it)
-                val destFile = File(dest, it)
-                // 递归复制
-                copyFolder(srcFile, destFile);
-            }
-        } else {
-            src.runCatching {
-                inputStream().use { input ->
-                    dest.apply {
-                        outputStream().use { output ->
-                            input.copyTo(output)
-                        }
-                    }
-                }
-            }.onFailure {
-                println("error: " + it.message)
-            }
-        }
-    }
+class UploadGithub : Plugin<Project> {
 
 
     override fun apply(project: Project) {
@@ -57,8 +28,6 @@ class UploadGithub : Plugin<Project> {
 
         project.afterEvaluate {
             val uploadConfig = project.extensions.findByName("upload") as UploadConfig
-
-            println(uploadConfig)
 
             val publishingExtension = Util.publishingExtension(project)
 
@@ -94,14 +63,11 @@ class UploadGithub : Plugin<Project> {
                 }
             }
 
-            var url = uploadConfig.url
-            if (url.isEmpty()) {
-                url = "build/repo"
-            }
+            val mavenUrl = "../build/repo"
 
             publishingExtension?.repositories {
                 it.maven { repo ->
-                    repo.url = project.file(url).toURI()
+                    repo.url = project.file(mavenUrl).toURI()
                 }
             }
 
@@ -112,9 +78,8 @@ class UploadGithub : Plugin<Project> {
             project.tasks.filter {
                 it.name == "publishMavenPublicationToMavenRepository"
             }.firstOrNull()?.doLast {
-                val aarFile = project.file(url)
-                println("\naar 路径: ${aarFile}")
-                // todo 开始上传 github
+                val aarFile = project.file(mavenUrl)
+                //  开始上传 github
                 uploadGithub(aarFile, uploadConfig, project)
             }
         }
@@ -132,17 +97,20 @@ class UploadGithub : Plugin<Project> {
         // 获取 repo 的名称
         val lastIndex = uploadConfig.githubURL.lastIndexOf("/")
         val lastIndex2 = uploadConfig.githubURL.lastIndexOf(".git")
-        val repo = uploadConfig.githubURL.substring(lastIndex + 1, lastIndex2)
+        val repoName = uploadConfig.githubURL.substring(lastIndex + 1, lastIndex2)
 
+
+        val buildDir = project.file("../build")
         // 将 aar copy 到 Maven 目录
-        val repoFile = File(project.buildDir.absolutePath + File.separator + repo)
+        val gitWorkDir = project.file("../build/$repoName")
 
         println("aarFile: " + aarFile.absoluteFile)
-        println("mavenFile: " + repoFile.absoluteFile)
+        println("mavenFile: " + gitWorkDir.absoluteFile)
 
-        if (!repoFile.exists()) {
+
+        if (!gitWorkDir.exists()) {
             project.exec {
-                it.workingDir = project.buildDir
+                it.workingDir = buildDir
                 if (uploadConfig.githubBranch.isEmpty()) {
                     it.commandLine("git", "clone", uploadConfig.githubURL)
                 } else {
@@ -150,58 +118,72 @@ class UploadGithub : Plugin<Project> {
                 }
             }
         }
-        // 将 aar 拷贝到 repo 目录
-        copyFolder(aarFile, repoFile)
 
-        val arrFilePath = uploadConfig.groupId.replace(".", File.separator) + File.separator + uploadConfig.artifactId
+        // 将 aar 拷贝到 repo 目录
+        FileUtils.copyFolder(aarFile, gitWorkDir)
+
+
+        val git = Git.open(gitWorkDir)
+
+        val arrName = uploadConfig.groupId.replace(".", File.separator) + File.separator + uploadConfig.artifactId
+
+        // git diff 比较，如果没有可提交的内容，则直接 return 不处理
+        val diffArray = git.diff().setDestinationPrefix(arrName).call()
+        println("diff=${diffArray}")
+        if (diffArray.size == 0) {
+            println("git diff $arrName 时发现 aar 文件没有可提交的内容")
+            return
+        }
+
+        // git add
+        git.add().addFilepattern(arrName).call()
+        // git commit
+        val dependency = "${uploadConfig.groupId}:${uploadConfig.artifactId}:${uploadConfig.version}"
+        git.commit().setMessage(dependency).call()
+
+
         // push 到 maven
         project.exec {
-            it.workingDir = File(project.buildDir.absolutePath + File.separator + repo)
-
-            println(" arrFilePath=${arrFilePath} ${it.workingDir.exists()}  ${it.workingDir}")
-
-            it.commandLine("git", "add", arrFilePath)
-            val dependency = "implementation '${uploadConfig.groupId}:${uploadConfig.artifactId}:${uploadConfig.version}"
-            it.commandLine("git", "commit-m", "添加 ${dependency}依赖")
+            it.workingDir = gitWorkDir
             if (uploadConfig.githubBranch.isEmpty()) {
                 it.commandLine("git", "push")
             } else {
+                println("git push origin ${uploadConfig.githubBranch}")
                 it.commandLine("git", "push", "origin", uploadConfig.githubBranch)
             }
+        }
 
-            // git@github.com:MRwangqi/Maven.git
-            // 获取 repo 的名称
-            val l = uploadConfig.githubURL.lastIndexOf(":")
-            val l2 = uploadConfig.githubURL.lastIndexOf("/")
-            val userName = uploadConfig.githubURL.substring(l + 1, l2)
 
-            println("已上传到 github=${uploadConfig.githubURL} 仓库")
-            println("""
-                maven 镜像源为:
+        // 获取 repo 的名称
+        val l = uploadConfig.githubURL.lastIndexOf(":")
+        val l2 = uploadConfig.githubURL.lastIndexOf("/")
+        val userName = uploadConfig.githubURL.substring(l + 1, l2)
+
+        println("""
+                已上传到 github=${uploadConfig.githubURL} 仓库, maven 镜像源为:
                 
                 maven{
-                   url "https://raw.githubusercontent.com/${userName}/${repo}/${uploadConfig.githubBranch}"
+                   url "https://raw.githubusercontent.com/${userName}/${repoName}/${uploadConfig.githubBranch}"
                 }
+                
+                可添加依赖使用:
+                implementation '${uploadConfig.groupId}:${uploadConfig.artifactId}:${uploadConfig.version}'
             """.trimIndent())
-            println("可添加依赖使用:\nimplementation '${uploadConfig.groupId}:${uploadConfig.artifactId}:${uploadConfig.version}'\n")
-        }
 
     }
 
 
-    private val scopeMapping = mapOf<String, String?>(
-            "api" to "compile",
-            "implementation" to "compile",
-            "compile" to "compile"
-    )
-
-    private fun applyPomDeps(pom: MavenPom, project: Project) {
+    private fun addPomDeps(pom: MavenPom, project: Project) {
+        val scopeMapping = mapOf<String, String?>(
+                "api" to "compile",
+                "implementation" to "compile",
+                "compile" to "compile"
+        )
         pom.withXml { xml ->
             val dependenciesNode = xml.asNode().appendNode("dependencies")
-            //Iterate over the compile dependencies (we don't want the test ones), adding a <dependency> node for each
             scopeMapping.keys.forEach { key ->
                 try {
-                    project.configurations.getByName(key).allDependencies?.forEach { dependency ->
+                    project.configurations.getByName(key).allDependencies.forEach { dependency ->
                         val dependencyNode = dependenciesNode.appendNode("dependency")
                         dependencyNode.appendNode("groupId", dependency.group)
                         dependencyNode.appendNode("artifactId", dependency.name)
